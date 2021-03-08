cf05871d916ebad71bcacc0e92ef76c657961525
Alex Sumenko <alexsumenko@Alexs-MacBook-Pro.local>
2021-03-08 14:00:35 +0200

    add fetch service and api base url

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL =
+    'https://test-shopping-list-61f37-default-rtdb.firebaseio.com/';
---
+++ src/utils/fetch.js
@@ -0,0 +1,18 @@
+import API_BASE_URL from './constants';
+
+export const httpRequest = (method, requestPath, requestData) => {
+    return fetch(`${API_BASE_URL}${requestPath}`, {
+        method,
+        body: JSON.stringify(requestData),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(res => {
+            if (res.status < 200 || res.status >= 300) {
+                throw res.statusText;
+            }
+            return res.json();
+        })
+        .catch(err => alert(err));
+};
